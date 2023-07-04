250e6e6832a3593e2e850d2f68a85f436de839a1
joysouza.contato@gmail.com <joysouza.contato@gmail.com>
2023-07-03 22:15:32 -0300

    Try e Catch

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,20 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    event.preventDefault();
-  
-    // Selecionando os elementos HTML relevantes
-    const nome = document.querySelector(".profile-name");
-    const user = document.querySelector(".profile-username");
-    const bio = document.querySelector(".profile-bio");
-    const repositorio = document.querySelector("#repository");
-    const followers = document.querySelector("#followers");
-    const following = document.querySelector("#following");
-    const avatar = document.querySelector("#avatar");
-    const link = document.querySelector("#link");
-  
-    // URL da API do GitHub para obter informações do usuário
-    const url = `https://api.github.com/users/jooysoouzaa`;
-  
+  event.preventDefault();
+
+  // Selecionando os elementos HTML relevantes
+  const nome = document.querySelector(".profile-name");
+  const user = document.querySelector(".profile-username");
+  const bio = document.querySelector(".profile-bio");
+  const repositorio = document.querySelector("#repository");
+  const followers = document.querySelector("#followers");
+  const following = document.querySelector("#following");
+  const avatar = document.querySelector("#avatar");
+  const link = document.querySelector("#link");
+
+  // URL da API do GitHub para obter informações do usuário
+  const url = `https://api.github.com/users/jooysoouzaa`;
+
+  try {
     // Fazendo a solicitação GET para a API do GitHub usando fetch
     fetch(url)
       .then(function (resposta) {
@@ -29,6 +30,12 @@ document.addEventListener("DOMContentLoaded", function (event) {
         following.innerText = json.following;
         avatar.src = json.avatar_url;
         link.href = json.html_url;
+      })
+      // Tratamento de erro
+      .catch(function (erro) {
+        alert("Ocorreu um erro ao buscar o endereço, tente novamente mais tarde.");
       });
-  });
-  
+  } catch (erro) {
+    console.log(erro);
+  }
+});
